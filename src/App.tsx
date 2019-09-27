64b62c19a2b71978d591041b2bfd107c32967381
yangbo <1832064870@qq.com>
2019-09-27 14:15:08 +0800

    hello world.

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View><Text>hello world.</Text></View>
+  );
+};
